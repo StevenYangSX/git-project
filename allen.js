045e7d3bd4b13c5cb2dc27975afc7ace68e4e91e
OkeyDokeyYooo <873262667a@gmail.com>
2019-12-14 12:19:01 -0800

    add allen.js

---
+++ allen.js
@@ -0,0 +1,19 @@
+var sizeOf = require('image-size');
+
+
+function ratio(imgPath, width){
+    var dimensions = sizeOf(imgPath);
+    return dimensions.width/dimensions.height;
+}
+
+function width(imgPath){
+    var dimensions = sizeOf(imgPath);
+    return dimensions.width;
+}
+
+function height(imgPath){
+    var dimensions = sizeOf(imgPath);
+    return dimensions.height;
+}
+
+module.exports = {ratio, width, height};

8f86aef723f4317353854d511c01991d4aa5a7c6
StevenYangSX <yangshixin1206@icloud.com>
2019-12-14 12:23:34 -0800

    added a filter function

--- steven.js
+++ steven.js
@@ -2,4 +2,10 @@ var nums = [1,2,3,4,5,6,7,8,9]
 
 var newNums = nums.map(element => element + 3)
 
-console.log(newNums)
+console.log(newNums);
+
+
+//added some new code
+var newNums.filter(ele=> ele === 3);
+condole.log(newNums);
+
